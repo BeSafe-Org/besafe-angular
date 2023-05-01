ac4d3ced970bb3677f22aba09309368f3d2a9ff0
ashishpol00 <ashishrameshpol4@gmail.com>
2023-05-01 12:19:38 +0530

    fix: auth component not loading while routing to it to it
    only signup loads

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { APP_ROUTES } from '../_shared/utils/routes';
+import { AuthComponent } from './auth.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 
 export const routes: Routes = [
     {
         path: APP_ROUTES.notSpecified,
+        component: AuthComponent,
         children: [
             { path: APP_ROUTES.notSpecified, redirectTo: APP_ROUTES.auth.signin._, pathMatch: 'full' },
             { path: APP_ROUTES.auth.signup._, component: SignupComponent },
