cf394bb3bc68c7cce3c5dda51b8cad3af36a6ccf
ashishpol00 <ashishrameshpol4@gmail.com>
2023-06-06 00:13:08 +0530

    feat: file grid and list view
    implemented ui of file grid and list view

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,17 +5,23 @@ import { HomeRoutingModule } from './home-routing.module';
 import { RecycleBinComponent } from './recycle-bin/recycle-bin.component';
 import { StarredFilesComponent } from './starred-files/starred-files.component';
 import { UltraSavedFilesComponent } from './ultra-saved-files/ultra-saved-files.component';
+import { FileGridCardComponent } from './my-files/file-grid-card/file-grid-card.component';
+import { FileListCardComponent } from './my-files/file-list-card/file-list-card.component';
+import { SharedModule } from '../_shared/shared.module';
 
 @NgModule({
     declarations: [
         MyFilesComponent,
         RecycleBinComponent,
         StarredFilesComponent,
-        UltraSavedFilesComponent
+        UltraSavedFilesComponent,
+        FileGridCardComponent,
+        FileListCardComponent
     ],
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        SharedModule
     ]
 })
 export class HomeModule { }
