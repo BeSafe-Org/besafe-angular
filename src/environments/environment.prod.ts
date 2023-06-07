0b75c0c5d7c9e7f98f7d4aeae34ed997a6c9e4d5
Eissa Faheem <eissafaheem@gmail.com>
2023-06-07 13:33:07 +0530

    Feat: Array Buffer add to blockchain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,59 +1,59 @@
 export const environment = {
     baseUrl: "http://localhost:8080",
     production: true,
-    address: '0xAf31aB68C422aAcE0a0868D98deDe5Fbba92B910',
+    address: '0xF8f10bf861d890408f35adA34e9aF208d1848901',
     abi: [
         {
-            "inputs": [] as any,
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "constructor"
+          "inputs": [] as any,
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "constructor"
         },
         {
-            "constant": true,
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "",
-                    "type": "string"
-                }
-            ],
-            "name": "files",
-            "outputs": [
-                {
-                    "internalType": "string",
-                    "name": "fileId",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "fileData",
-                    "type": "string"
-                }
-            ],
-            "payable": false,
-            "stateMutability": "view",
-            "type": "function"
+          "constant": true,
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "name": "files",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "fileId",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "fileData",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
         },
         {
-            "constant": false,
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "_fileId",
-                    "type": "string"
-                },
-                {
-                    "internalType": "string",
-                    "name": "_fileData",
-                    "type": "string"
-                }
-            ],
-            "name": "addFile",
-            "outputs": [],
-            "payable": false,
-            "stateMutability": "nonpayable",
-            "type": "function"
+          "constant": false,
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "_fileId",
+              "type": "string"
+            },
+            {
+              "internalType": "string",
+              "name": "_fileData",
+              "type": "string"
+            }
+          ],
+          "name": "addFile",
+          "outputs": [],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
         }
-    ]
+      ],
 };
