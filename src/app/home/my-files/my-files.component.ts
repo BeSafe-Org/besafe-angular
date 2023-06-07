98fe381a6b5ec623a29d23bbc87e2b7418c6512d
ashishpol00 <ashishrameshpol4@gmail.com>
2023-06-07 14:48:48 +0530

    feat: fetch file api implemented in my files

--- src/app/home/my-files/my-files.component.ts
+++ src/app/home/my-files/my-files.component.ts
@@ -1,11 +1,11 @@
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { AddFilesModalPopupComponent } from './add-files-modal-popup/add-files-modal-popup.component';
-import { FILE_OBJ } from './temp';
 import { BesafeGlobalService, FileViewType } from 'src/app/_shared/services/besafe-global.service';
 import { FILE_SYSTEM_OPERATION_CONTAINER_ID, FileSystemOperationsDirective, FileUI } from '../_shared/directives/file-system-operations/file-system-operations.directive';
 import { Subscription } from 'rxjs';
 import { FILE_ID_PREFIX } from '../_shared/utils/file-id-prefix';
 import { ContextMenuComponent, ContextMenuPointerEventPosition } from '../_shared/components/context-menu/context-menu.component';
+import { GoogleApiService, UserInfo } from 'src/app/_core/services/backend/google-api.service';
 
 @Component({
     selector: 'app-my-files',
@@ -15,7 +15,7 @@ import { ContextMenuComponent, ContextMenuPointerEventPosition } from '../_share
 export class MyFilesComponent implements OnInit, OnDestroy {
     @ViewChild('operationResult') operationResult: FileSystemOperationsDirective;
 
-    public viewType: FileViewType;
+    public viewType: FileViewType; userInfo?: UserInfo
     public viewTypeSubscription: Subscription;
     public allFiles: FileUI[] = [];
     public readonly fileIdPrefix: string = FILE_ID_PREFIX.allFiles;
@@ -24,8 +24,13 @@ export class MyFilesComponent implements OnInit, OnDestroy {
     constructor(
         private componentFactoryResolver: ComponentFactoryResolver,
         private viewContainerRef: ViewContainerRef,
-        private besafeGlobalService: BesafeGlobalService
-    ) { }
+        private besafeGlobalService: BesafeGlobalService,
+        private googleApi: GoogleApiService
+    ) {
+        googleApi.userProfileSubject.subscribe(info => {
+            this.userInfo = info
+        })
+    }
 
     ngOnInit(): void {
         this.initializeViewTypeObserver();
@@ -41,15 +46,23 @@ export class MyFilesComponent implements OnInit, OnDestroy {
     }
 
     private getAllFiles(): void {
-        FILE_OBJ.files.forEach(file => {
-            this.allFiles.push({
-                id: file.id,
-                name: file.name,
-                mimeType: file.mimeType
-            })
-        });
-        this.allFiles = [...this.allFiles];
-        this.operationResult.setNoOfGridColumns();
+        this.googleApi.getAllFiles().subscribe(
+            (response: any) => {
+                console.log('All files retrieved successfully:', response);
+                (response.files as any[]).forEach(file => {
+                    this.allFiles.push({
+                        id: file.id,
+                        name: file.name,
+                        mimeType: file.mimeType
+                    })
+                });
+                this.allFiles = [...this.allFiles];
+                this.operationResult.setNoOfGridColumns();
+            },
+            (error) => {
+                console.log('Error retrieving all files:', error);
+            }
+        );
     }
 
     public toggleViewType(): void {
