4a850b79f23bcbaa77cba25a5f326533a4b0ef81
Eissa Faheem <eissafaheem@gmail.com>
2023-06-24 17:30:16 +0530

    Feat: AES encrypt blob conversion

--- src/app/_core/client/managementClient/DriveOperationClient.ts
+++ src/app/_core/client/managementClient/DriveOperationClient.ts
@@ -25,12 +25,16 @@ export class DriveOperationClient {
             const encryptedContent = await new AesCrypto().encryptAES(fileContent, this.password);
             const encryptedBlob = new Blob([encryptedContent], { type: 'application/octet-stream' });
             let res = await new DriveOperationBackEndClient(this.oAuthService, this.httpClient).uploadFile(encryptedBlob, file)
-            if (isUltraSecure) {
-                // Upload on Blockchain
-            }
             uploadResult.id = res.id;
             uploadResult.name = res.name;
             uploadResult.mimeType = res.mimeType;
+            if (isUltraSecure) {
+                const fileReader = new FileReader();
+
+                
+                    await new SmartContracts().addFile(uploadResult.id, encryptedBlob);
+                  
+            }
             resolve(uploadResult);
         })
     }
@@ -47,7 +51,7 @@ export class DriveOperationClient {
             console.log("ArrayBuffer to send: ", arrayBuffer);
         };
         reader.readAsArrayBuffer(file);
-        let res = await new SmartContracts().addFile(fileId, arrayBuffer)
+        // let res = await new SmartContracts().addFile(fileId, arrayBuffer)
     }
 
     downloadFile(fileId: string, fileName: string): Promise<Result> {
@@ -60,6 +64,10 @@ export class DriveOperationClient {
                     let decrptedarraybuffer;
                     new Conversion().convertBlobToArrayBuffer(response)
                         .then(async arrayBuffer => {
+
+                            let encryptedUltraSafeFile = await new SmartContracts().getFile(fileId);
+                            console.log("encryptedUltraSafeFile", encryptedUltraSafeFile);
+                            
                             decrptedarraybuffer = arrayBuffer;
                             console.log("arrayBuffer: ", arrayBuffer);
                             const decryptedArrayBuffer = await new AesCrypto().decryptAES(decrptedarraybuffer, this.password);
